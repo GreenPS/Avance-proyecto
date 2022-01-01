7b2ab49da4d2531cfe219e3740ee8676abd152d4
camilonfs1 <camilonfs1@gmail.com>
2022-01-01 17:58:40 -0500

    :sparkles: front

---
+++ app/src/main/java/com/example/greenps/View/UserPass.kt
@@ -0,0 +1,12 @@
+package com.example.greenps.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.greenps.R
+
+class UserPass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_pass)
+    }
+}
